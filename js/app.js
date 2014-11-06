f7d5ba7b8a8ca4827d378807240ba1c5cba0c59d
Ivan Skiridomov <ivan.skiridomov@nexetic.com>
2014-11-07 02:05:38 +0300

    refactor and remove logging from app.js

--- js/app.js
+++ js/app.js
@@ -1,27 +1,16 @@
+
 var boldApplier;
 
-function gEBI(id) {
-    return document.getElementById(id);
-}
-
 function toggleBold() {
-    console.log("bold in function");
     boldApplier.toggleSelection();
 }
 
 $().ready(function(){
-    console.log("jquery started");
-    rangy.init()
-
+    rangy.init();
     boldApplier = rangy.createCssClassApplier("bold");
-    console.log(boldApplier);
-
 
     $("#toggleBoldButton").on('click',function(){
-        var selection = rangy.getSelection();
-        console.log(selection);
         toggleBold();
-        return false;
     });
 
 });
