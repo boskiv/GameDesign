8432ab79708f37341e241c71d9a3fd54ed9d4d17
Ivan Skiridomov <ivan.skiridomov@nexetic.com>
2014-11-07 12:52:18 +0300

    Add italic and underline toggle

--- js/app.js
+++ js/app.js
@@ -1,16 +1,32 @@
 
-var boldApplier;
+var boldApplier, italicApplier,underlineApplier;
 
 function toggleBold() {
     boldApplier.toggleSelection();
+}
+function toggleItalic() {
+    italicApplier.toggleSelection();
+}
+function toggleUnderline() {
+    underlineApplier.toggleSelection();
 }
 
 $().ready(function(){
     rangy.init();
     boldApplier = rangy.createCssClassApplier("bold");
+    italicApplier = rangy.createCssClassApplier("italic");
+    underlineApplier = rangy.createCssClassApplier("underline");
 
     $("#toggleBoldButton").on('click',function(){
         toggleBold();
+    });
+
+    $("#toggleItalicButton").on('click',function(){
+        toggleItalic();
+    });
+
+    $("#toggleUnderlineButton").on('click',function(){
+        toggleUnderline();
     });
 
 });
