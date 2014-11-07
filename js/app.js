15769cec07e2bb36b6f25fc5ef1abb08ddf1f3a0
Ivan Skiridomov <ivan.skiridomov@nexetic.com>
2014-11-07 13:27:12 +0300

    add font style feature

--- js/app.js
+++ js/app.js
@@ -1,14 +1,45 @@
 
 var boldApplier, italicApplier,underlineApplier;
+var arialApplier, comicApplier, georgiaApplier, myriadApplier, ptApplier, timesApplier, verdanaApllier;
 
 function toggleBold() {
     boldApplier.toggleSelection();
 }
+
 function toggleItalic() {
     italicApplier.toggleSelection();
 }
+
 function toggleUnderline() {
     underlineApplier.toggleSelection();
+}
+
+function toggleArial() {
+    arialApplier.toggleSelection();
+}
+
+function toggleComic() {
+    comicApplier.toggleSelection();
+}
+
+function toggleGeorgia() {
+    georgiaApplier.toggleSelection();
+}
+
+function toggleMyriad() {
+    myriadApplier.toggleSelection();
+}
+
+function togglePt() {
+    ptApplier.toggleSelection();
+}
+
+function toggleTimes() {
+    timesApplier.toggleSelection();
+}
+
+function toggleVerdana() {
+    verdanaApllier.toggleSelection();
 }
 
 $().ready(function(){
@@ -16,6 +47,15 @@ $().ready(function(){
     boldApplier = rangy.createCssClassApplier("bold");
     italicApplier = rangy.createCssClassApplier("italic");
     underlineApplier = rangy.createCssClassApplier("underline");
+    arialApplier = rangy.createCssClassApplier("arial");
+    comicApplier = rangy.createCssClassApplier("comic");
+    georgiaApplier = rangy.createCssClassApplier("georgia");
+    myriadApplier = rangy.createCssClassApplier("myriad");
+    ptApplier = rangy.createCssClassApplier("pt");
+    timesApplier = rangy.createCssClassApplier("times");
+    verdanaApllier = rangy.createCssClassApplier("verdana");
+
+
 
     $("#toggleBoldButton").on('click',function(){
         toggleBold();
@@ -27,6 +67,34 @@ $().ready(function(){
 
     $("#toggleUnderlineButton").on('click',function(){
         toggleUnderline();
+    });
+
+    $("#toggleArialButton").on('click',function(){
+        toggleArial();
+    });
+
+    $("#toggleComicButton").on('click',function(){
+        toggleComic();
+    });
+
+    $("#toggleGeorgiaButton").on('click',function(){
+        toggleGeorgia();
+    });
+
+    $("#toggleMyriadButton").on('click',function(){
+        toggleMyriad();
+    });
+
+    $("#togglePtButton").on('click',function(){
+        togglePt();
+    });
+
+    $("#toggleTimesButton").on('click',function(){
+        toggleTimes();
+    });
+
+    $("#toggleVerdanaButton").on('click',function(){
+        toggleVerdana();
     });
 
 });
